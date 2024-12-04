e64ff29fb20335a7d2ed17031148a2df7203e4e9
Jay <jay@Jays-MBP.home>
2024-12-04 20:00:27 +0100

    gitignore and podman to docker migration

--- start_django.sh
+++ start_django.sh
@@ -7,10 +7,6 @@ poetry run python manage.py migrate
 # create superuser
 poetry run python manage.py createsuperuser --username jenny --email jlo@acme.com --noinput
 
-# wait for Neo4J: podman compose does not respect the health check
-echo "Waiting for Neo4J"
-sleep 20
-
 # create demo data in Django and Neo4j
 echo "Setting up demo data"
 poetry run python manage.py demodata
